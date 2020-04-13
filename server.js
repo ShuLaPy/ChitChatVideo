ae9d19f51ca0cec3f781663f5bd444134d658ca8
ShuLaPy <shubhamlad1001@gmail.com>
2020-04-14 02:40:48 +0530

    working p2p video app

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const path = require("path");
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "/public")));
+let clients = 0;
+
+io.on("connection", function (socket) {
+  socket.on("NewClient", function () {
+    if (clients < 2) {
+      if (clients == 1) this.emit("CreatePeer");
+    } else {
+      this.emit("SessionActive");
+    }
+    clients++;
+  });
+
+  socket.on("Offer", SendOffer);
+  socket.on("Answer", SendAnswer);
+  socket.on("disconnect", Disconnect);
+});
+
+function Disconnect() {
+  if (clients > 0) {
+    clients--;
+    this.broadcast.emit("RemoveVideo");
+  }
+}
+
+function SendOffer(offer) {
+  this.broadcast.emit("BackOffer", offer);
+}
+
+function SendAnswer(data) {
+  this.broadcast.emit("BackAnswer", data);
+}
+
+http.listen(PORT, () => console.log(`Running on port ${PORT}`));
